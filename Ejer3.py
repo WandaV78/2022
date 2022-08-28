41a0e1a30a0300f6700846c27d434ad5bd1795f1
WandaV78 <60580682+WandaV78@users.noreply.github.com>
2022-08-28 12:52:10 -0300

    Add files via upload

---
+++ Ejer3.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Wan
+#
+# Created:     28/08/2022
+# Copyright:   (c) Wan 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#solicito se ingrese nombre por pantalla
+nombre= input ("Ingrese su nombre: ")
+
+#solicito se ingrese DNI por pantalla
+dni = int(input ("Ingrese su DNI: "))
+
+#muestro datos ingresados concatenados
+print(f'Su nombre es {nombre}' + f' y su DNI es:{dni}')
+
+
