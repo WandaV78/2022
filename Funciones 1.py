40525731072b6f6431d0231591dcb97e603851d2
WandaV78 <60580682+WandaV78@users.noreply.github.com>
2022-09-18 20:41:21 -0300

    Add files via upload

---
+++ Funciones 1.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+#Realizar una función que se llame area_rectangulo() que devuelva el área del
+#cuadrado a partir de su base y altura.
+#-------------------------------------------------------------------------------
+
+
+
+def area_rectangulo (base, altura):
+    return ( base * altura)
+
+print (area_rectangulo (4,4))
+
+
+
