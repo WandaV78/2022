0360c70dd6e735d2575a09f0a58779cc610c65b3
WandaV78 <60580682+WandaV78@users.noreply.github.com>
2022-09-18 19:38:55 -0300

    Add files via upload

---
+++ Bucle 4.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+'''
+Definir una lista
+Contar los elementos de esa lista
+Al finalizar mostrar por pantalla la cantidad de elementos de la lista
+No utilizar función len
+'''
+
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+    lista = [5,85,74,10]
+    contador = 0
+
+
+    for i in lista:
+        contador = contador +1
+
+    print(f'La cantidad de numeros de la lista es : {contador}')
+
+
+
+if __name__ == '__main__':
+    main()
